54ff424d63f2cee71f5272cd8252c52d08b97838
hengmhs <hengmhs@gmail.com>
2023-04-06 05:05:08 +0800

    Add HSK select menu and HSK JSON imports

--- src/HSKSelect.js
+++ src/HSKSelect.js
@@ -9,7 +9,12 @@ class HSKSelect extends React.Component {
     return (
       <div>
         <h1>HSK Level Select</h1>
-        <Button onClick={this.props.chooseLevel}>HSK Level 1</Button>
+        <Button onClick={this.props.chooseLevell}>HSK Level 1</Button>
+        <Button onClick={this.props.chooseLevel2}>HSK Level 2</Button>
+        <Button onClick={this.props.chooseLevel3}>HSK Level 3</Button>
+        <Button onClick={this.props.chooseLevel4}>HSK Level 4</Button>
+        <Button onClick={this.props.chooseLevel5}>HSK Level 5</Button>
+        <Button onClick={this.props.chooseLevel6}>HSK Level 6</Button>
       </div>
     );
   }

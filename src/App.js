0f3d98fade50d0a40a852e1a89153a4d41497465
hengmhs <hengmhs@gmail.com>
2023-04-09 14:58:07 +0800

    Add bootstrap styling with grid system and navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import WagerButtons from "./WagerButtons";
 import Game from "./Game";
 import Menu from "./Menu";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
   // TODO: getRandomChar does not choose words that already have been chosen
@@ -53,22 +58,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        {" "}
-        <div className="header">
-          <div>Scholar's Wager</div>
-          <Button onClick={this.goToMainMenu} className="btn btn-dark">
-            Main Menu
-          </Button>
-        </div>
-        <div className="main-display">
-          <Menu
-            isMainMenu={this.state.isMainMenu}
-            gameIsRunning={this.state.gameIsRunning}
-            isRevision={this.state.isRevision}
-            startGame={this.startGame}
-            startRevision={this.startRevision}
-          />
-        </div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Scholar's Wager</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link onClick={this.goToMainMenu}>Menu</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container fluid>
+          <Row>
+            <Col lg={2}></Col>
+            <Col lg={8} className="main-display">
+              <Menu
+                isMainMenu={this.state.isMainMenu}
+                gameIsRunning={this.state.gameIsRunning}
+                isRevision={this.state.isRevision}
+                startGame={this.startGame}
+                startRevision={this.startRevision}
+              />
+              <Col lg={2}></Col>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
